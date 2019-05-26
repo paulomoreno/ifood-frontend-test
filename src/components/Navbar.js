c830e9c15a4a582f42b8299bb41b3cb160a4dffa
Paulo Moreno <paulomorenoo@gmail.com>
2019-05-26 20:59:34 -0300

    feat: update logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,19 @@
 import React, { useEffect } from "react";
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import './navbar.css';
 
-import {getUser, clearUser} from '../modules/user/userActions';
+import { getUser, clearUser } from '../modules/user/userActions';
 
 function CustomNavbar({ token, user, getUser, logout }) {
   useEffect(() => {
     getUser();
-  },[]);
+  }, []);
 
   const logoutOnClick = (e) => {
     e.preventDefault();
@@ -25,20 +26,35 @@ function CustomNavbar({ token, user, getUser, logout }) {
       <Navbar.Brand href="#home">Spotifood</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
-        {token && user && (
-          <Navbar.Text>
-            {user.images && user.images.length > 0 && (
-              <Image className="userImage" src={user.images[0].url} roundedCircle />
-            )}
-            <b>{user.display_name}</b>
-            <Button onClick={logoutOnClick} variant="link">Logout</Button>
-          </Navbar.Text>
-        )}
+        <Nav>
+          {token && user && (
+            <NavDropdown title={
+              <span>
+                {user.images && user.images.length > 0 && (
+                  <Image className="userImage" src={user.images[0].url} roundedCircle />
+                )}
+                <b>{user.display_name}</b>
+              </span>
+            } id="collasible-nav-dropdown">
+              <NavDropdown.Item onClick={logoutOnClick}>Logout</NavDropdown.Item>
+            </NavDropdown>
+
+          )}
+        </Nav>
       </Navbar.Collapse>
     </Navbar >
 
   );
 }
+
+{/* <Navbar.Text>
+            {user.images && user.images.length > 0 && (
+              <Image className="userImage" src={user.images[0].url} roundedCircle />
+            )}
+            <b>{user.display_name}</b>
+            <Button onClick={logoutOnClick} variant="link">Logout</Button>
+          </Navbar.Text> */}
+
 
 function mapStateToProps(state) {
   return {
