842653adbaabd05557bea138d9ad9a9a37b8d189
Paulo Moreno <paulomorenoo@gmail.com>
2019-06-01 00:30:26 -0300

    Updates

--- precache-manifest.bfef76b3897bea33a3668c68148bb684.js
+++ precache-manifest.bfef76b3897bea33a3668c68148bb684.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f1414896251b66b3e9de7b0418c75f71",
+    "revision": "bcf07cbc0854747855ff7096833546bc",
     "url": "/ifood-frontend-test/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ifood-frontend-test/static/css/2.dae6fbb9.chunk.css"
   },
   {
-    "revision": "3710a9c12232b3ee4aa9",
+    "revision": "2d7d2aaf8f7b541515c8",
     "url": "/ifood-frontend-test/static/css/main.d32d8115.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ifood-frontend-test/static/js/2.ee59be88.chunk.js"
   },
   {
-    "revision": "3710a9c12232b3ee4aa9",
-    "url": "/ifood-frontend-test/static/js/main.26c093f3.chunk.js"
+    "revision": "2d7d2aaf8f7b541515c8",
+    "url": "/ifood-frontend-test/static/js/main.41b3a17f.chunk.js"
   },
   {
     "revision": "03fb1823aa76949644b5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ifood-frontend-test/precache-manifest.2687b136c71540dac9af63eafa0ed894.js"
+  "/ifood-frontend-test/precache-manifest.bfef76b3897bea33a3668c68148bb684.js"
 );
 
 self.addEventListener('message', (event) => {
