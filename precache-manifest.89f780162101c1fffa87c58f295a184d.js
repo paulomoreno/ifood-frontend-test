5f78c577d63241a29bae0c5bd230872320acbda1
Paulo Moreno <paulomorenoo@gmail.com>
2019-06-01 00:04:20 -0300

    Updates

---
+++ precache-manifest.89f780162101c1fffa87c58f295a184d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7bb7443659804a4e73d15930772664",
+    "url": "/ifood-frontend-test/index.html"
+  },
+  {
+    "revision": "c8fd53c98c8fd83967b4",
+    "url": "/ifood-frontend-test/static/css/2.dae6fbb9.chunk.css"
+  },
+  {
+    "revision": "bd2dfaf5d327bc5dd95b",
+    "url": "/ifood-frontend-test/static/css/main.d32d8115.chunk.css"
+  },
+  {
+    "revision": "c8fd53c98c8fd83967b4",
+    "url": "/ifood-frontend-test/static/js/2.ee59be88.chunk.js"
+  },
+  {
+    "revision": "bd2dfaf5d327bc5dd95b",
+    "url": "/ifood-frontend-test/static/js/main.1090db5d.chunk.js"
+  },
+  {
+    "revision": "03fb1823aa76949644b5",
+    "url": "/ifood-frontend-test/static/js/runtime~main.1301fedf.js"
+  }
+]);
