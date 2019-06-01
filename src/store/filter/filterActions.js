aa81bcc11e6da6fcc7fadcf44c187f8855e0e30b
Paulo Moreno <paulomorenoo@gmail.com>
2019-06-01 10:35:09 -0300

    fix: update mocky url to use https

--- src/store/filter/filterActions.js
+++ src/store/filter/filterActions.js
@@ -4,7 +4,7 @@ import { validations } from '../../helpers/validations';
 import { getPlaylists } from '../playlist/playlistActions';
 import moment from 'moment';
 
-const FILTER_URL = 'http://www.mocky.io/v2/5a25fade2e0000213aa90776';
+const FILTER_URL = 'https://www.mocky.io/v2/5a25fade2e0000213aa90776';
 
 export const loading = () => {
   return {
